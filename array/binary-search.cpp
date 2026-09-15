bab3377c0d685a35defc08db1b7c2f872e159f9e
Lubna Fathima Shaik <lubnafathimashaik@gmail.com>
2026-09-15 12:39:49 +0530

    LC-binary-search [Runtime: N/A; Memory: N/A]

--- array/binary-search.cpp
+++ array/binary-search.cpp
@@ -1,23 +1,26 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        int low = 0;
-        int high = nums.size() - 1;
 
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
+    int binarySearch(vector<int>& nums, int low, int high, int target) {
 
-            if (nums[mid] == target) {
-                return mid;
-            }
-            else if (nums[mid] > target) {
-                high = mid - 1;
-            }
-            else {
-                low = mid + 1;
-            }
+        if (low > high) {
+            return -1;
         }
 
-        return -1;
+        int mid = low + (high - low) / 2;
+
+        if (nums[mid] == target) {
+            return mid;
+        }
+
+        if (target > nums[mid]) {
+            return binarySearch(nums, mid + 1, high, target);
+        }
+
+        return binarySearch(nums, low, mid - 1, target);
+    }
+
+    int search(vector<int>& nums, int target) {
+        return binarySearch(nums, 0, nums.size() - 1, target);
     }
 };
