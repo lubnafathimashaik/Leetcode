8f1fba5cd0fc5cd79f9492c5d218652ff526de3d
Lubna Fathima Shaik <lubnafathimashaik@gmail.com>
2026-09-17 20:01:35 +0530

    LC-majority-element [Runtime: N/A; Memory: N/A]

--- sorting/majority-element.cpp
+++ sorting/majority-element.cpp
@@ -1,62 +1,20 @@
-#include <bits/stdc++.h>
-using namespace std;
-
 class Solution {
 public:
-    // Function to find the majority element in an array
-    int majorityElement(vector<int>& nums) {
-        
-        // Size of the given array
-        int n = nums.size();
-        
-        // Count
-        int cnt = 0;
-        
-        // Element
-        int el; 
-        
-        // Applying the algorithm
-        for (int i = 0; i < n; i++) {
-            if (cnt == 0) {
-                cnt = 1;
-                el = nums[i];
-            } else if (el == nums[i]) {
-                cnt++;
-            } else {
-                cnt--;
+    int majorityElement(vector<int>& nums) {        for(int val:nums)
+        {
+            int f=0;
+            for(int e:nums)
+            {
+                if(e==val)
+                {
+                    f++;
+                }
+            }
+            if(f>nums.size()/2)
+            {
+                return val;
             }
         }
-        
-        /* Checking if the stored element
-         is the majority element*/
-        int cnt1 = 0;
-        for (int i = 0; i < n; i++) {
-            if (nums[i] == el) {
-                cnt1++;
-            }
-        }
-        
-        //return element if it is a majority element
-        if (cnt1 > (n / 2)) {
-            return el;
-        }
-        
-        //return -1 if no such element found
         return -1;
     }
-
-
-int main() {
-    vector<int> arr = {2, 2, 1, 1, 1, 2, 2};
-    
-    // Create an instance of Solution class
-    Solution sol;
-
-    int ans = sol.majorityElement(arr);
-    
-    // Print the majority element found
-    cout << "The majority element is: " << ans << endl;
-    
-    return 0;
-}
 };
